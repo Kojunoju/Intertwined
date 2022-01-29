be765474d83d5f51652be9803c488c20868952e5
StinkleDinkle <aidanhallaudio@gmail.com>
2022-01-29 16:51:04 +0000

    Footstep code and sound script

--- IntertwinedGameJam/Assets/Scripts/PlayerMovement.cs
+++ IntertwinedGameJam/Assets/Scripts/PlayerMovement.cs
@@ -11,13 +11,69 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
 
     Vector2 movement;
+
+    public FMODUnity.StudioEventEmitter Steps;
+    
+    public bool stepsPlaying = false;
+
+    public float timerThresh = 1;
+
+    public float t;
+
+
+
+
+
+
+
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         restart();
+        
+        if(stepsPlaying == false)
+        {
+            if(Input.GetKey(KeyCode.W))
+                {
+                    Steps.SendMessage("Play");
+                    stepsPlaying = true;
+                }
+        }
+      
+      if(stepsPlaying == false)
+        {
+            if(Input.GetKey(KeyCode.A))
+                {
+                    Steps.SendMessage("Play");
+                    stepsPlaying = true;
+                }
+        }
+       if(stepsPlaying == false)
+        {
+            if(Input.GetKey(KeyCode.S))
+                {
+                    Steps.SendMessage("Play");
+                    stepsPlaying = true;
+                }
+        }
+       
+       if(stepsPlaying == false)
+        {
+            if(Input.GetKey(KeyCode.D))
+                {
+                    Steps.SendMessage("Play");
+                    stepsPlaying = true;
+                }
+        }
+        timer();
+        
+       
+        
+     
     }
+
 
     void restart()
     {
@@ -26,6 +82,21 @@ public class PlayerMovement : MonoBehaviour
             SceneManager.LoadScene("Level1");
         }
     }
+
+    void timer()
+    {
+        if(stepsPlaying)
+        {
+            t += Time.deltaTime;
+            if(t>=timerThresh)
+            {
+                stepsPlaying = false;
+                t = 0f;
+            }
+        }
+    }
+
+
 
     private void FixedUpdate() 
     {
